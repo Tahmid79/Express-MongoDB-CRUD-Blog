e18bab87092f49f91347b3d9554c8830bdb04b39
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-09-03 20:19:54 +0600

    1st Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express') ;
+const app = express() ;
+const PORT = process.env.port ||  3000  ;
+const routes = require('./src/routes/crmRoutes') ;
+const mongoose = require('mongoose') ;
+
+//routes(app) ;
+
+mongoose.connect('mongodb://localhost/test',
+    { useNewUrlParser: true }) ;
+
+
+
+app.get('/' , function (req, res , next){
+   console.log('Request Method : ' , req.method) ;
+    next() ;
+} , function (req , res, next) {
+    console.log('Request Original URL : ', req.originalUrl  ) ;
+    next() ;
+} ,  function(req , res , next){
+    res.send('Request was successful') ;
+
+}  );
+
+
+app.use(routes) ;
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server is Running on Port: ${PORT}`)  ;
+}) ;
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,24 @@
+const routes = (app) =>{
+
+    app.route('/')
+
+        .get( (req , res ) =>{
+        res.send('This is the GET method') ;
+    })
+
+        .post( (req , res) => {
+        res.send('This is the Post method') ;
+    })
+
+        .put( (req , res) => {
+        res.send('This is the PUT method') ;
+    })
+
+        .delete( (req , res) => {
+        res.send('This is the Delete method') ;
+    })
+
+};
+
+module.exports = routes ;
+
